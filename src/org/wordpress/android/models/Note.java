f927924c6d5558d164a5e542b00669b425e761ef
Dan Roundhill <dan@automattic.com>
2014-05-08 16:23:12 -0700

    Changed 'unread' to 'read' to match model. Removed old REST API methods that are no longer used.

--- src/org/wordpress/android/models/Note.java
+++ src/org/wordpress/android/models/Note.java
@@ -275,45 +275,18 @@ public class Note extends Syncable {
         return !isRead();
     }
 
-    /**
-     * A note can have an "unread" of 0 or more ("likes" can have unread of 2+) to indicate the
-     * quantity of likes that are "unread" within the single note. So for a note to be "read" it
-     * should have "0"
-     */
-    Boolean isRead(){
-        return getUnreadCount().equals("0");
+    Boolean isRead() {
+        return queryJSON("read", 0) == 1;
     }
 
-    /**
-     * Sets the note's read count to "0" and saves it to sync with Simperium
-     */
     public void markAsRead() {
         try {
-            mNoteJSON.put("unread", "0");
+            mNoteJSON.put("read", 1);
         } catch (JSONException e) {
-            Log.e(TAG, "Unable to update note unread property", e);
+            Log.e(TAG, "Unable to update note read property", e);
             return;
         }
         save();
-    }
-
-    /**
-     * For some reason the unread count is a string in the JSON API but is truly represented
-     * by an Integer. We can handle a simple string.
-     */
-    public String getUnreadCount(){
-        return queryJSON("unread", "0");
-    }
-
-    /**
-     *
-     */
-    public void setUnreadCount(String count){
-        try {
-            mNoteJSON.putOpt("unread", count);
-        } catch (JSONException e){
-            AppLog.e(T.NOTIFS, "Failed to set unread property", e);
-        }
     }
 
     public Reply buildReply(String content){
